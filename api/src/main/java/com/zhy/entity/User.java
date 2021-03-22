5fd6316b52a6d03dddbc26b59ad1db35470da8f5
zhy <18306425328@163.com>
2021-03-22 16:11:24 +0800

    需部署jar包修改启动项

--- api/src/main/java/com/zhy/entity/User.java
+++ api/src/main/java/com/zhy/entity/User.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * dubbo服务间的应用即跨服务器之间的服务调用
+ * 调用过程中通过数据网络传输模拟信号。（网卡称数模转换器，转换两者需要的信号）
+ * Java中，只要实体类序列化，jvm会把pojo转换成对应的信号进行传输。
+ */
+
 @Data
 public class User implements Serializable {
 
--- privider/src/main/java/com/zhy/PrividerApplication.java
+++ privider/src/main/java/com/zhy/PrividerApplication.java
@@ -2,13 +2,15 @@ package com.zhy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @ImportResource({"classpath:config/privider.xml"})
-public class PrividerApplication {
+public class PrividerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args){
         SpringApplication.run(PrividerApplication.class,args);
@@ -17,5 +19,11 @@ public class PrividerApplication {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return super.configure(builder);
+        return builder.sources(PrividerApplication.class);
     }
 }
