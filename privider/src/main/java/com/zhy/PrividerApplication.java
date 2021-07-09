94ed895dc4075685a6a85631bf7109adbdcdd669
zhy <18306425328@163.com>
2021-07-09 18:33:38 +0800

    添加springboot启动注解

--- privider/src/main/java/com/zhy/PrividerApplication.java
+++ privider/src/main/java/com/zhy/PrividerApplication.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.ImportResource;
 
 import java.io.IOException;
 
+/**
+ * springboot默认情况下是自带了嵌入式容器spring-boot-starter-tomcat，如果需要构建war文件并进行部署时需要需改启动类，继承SpringBootServletInitializer,
+ * 并重写configure方法，打成war包放到tomcat中启动即可。
+ */
 @SpringBootApplication
 @ImportResource({"classpath:config/privider.xml"})
 public class PrividerApplication extends SpringBootServletInitializer {
